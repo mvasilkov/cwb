467ff8b141fb4605dc5c488b181ce690e439e1eb
Mark Vasilkov <mvasilkov@gmail.com>
2019-04-11 18:58:16 +0300

    Singleton

--- cwb.js
+++ cwb.js
@@ -1,7 +1,22 @@
+const EventEmitter = require('wolfy87-eventemitter')
+
 const ObjectId = require('./objectid')
 const { sleep, lock } = require('./lock')
 
+const cache = Object.create(null)
+
+class Channel extends EventEmitter {
+    constructor(key) {
+        if (cache[key]) return cache[key]
+
+        cache[key] = this
+    }
+}
+
 module.exports = {
+    __esModule: true,
+    default: Channel,
+    EventEmitter,
     ObjectId,
     sleep,
     lock,
