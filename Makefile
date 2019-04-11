2a94f4529e90980cf8d65f014722fe9328d05342
Mark Vasilkov <mvasilkov@gmail.com>
2019-04-12 02:21:26 +0300

    Cleanup-2

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 .PHONY: cwb
 cwb:
 	webpack ./cwb.js -o ./build/cwb.js --output-library cwb \
-	--mode production --target web --display-modules
+		--mode production --target web --display-modules
+	node build/clean.js
---
+++ build/clean.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const path = require('path')
+
+const cwb = path.resolve(__dirname, 'cwb.js')
+let a = fs.readFileSync(cwb, { encoding: 'utf8' })
+a = a.replace(/\n*^\/\*!.*?\*\/\n*/gsm, b => {
+    console.log(`Cleaning up: '''${b}'''`)
+    return ''
+})
+fs.writeFileSync(cwb, a, { encoding: 'utf8' })
