c6d8dc95c72bd071cd2fc18807dda15c77b2d6c8
Mark Vasilkov <mvasilkov@gmail.com>
2019-04-15 19:22:28 +0300

    Cleanup

--- cwb.js
+++ cwb.js
@@ -5,6 +5,8 @@ const { sleep, lock } = require('./lock')
 
 const cache = Object.create(null)
 
+const shiftInitial = t => Math.floor(0.5 * t * (Math.random() + 1))
+
 class Channel extends EventEmitter {
     constructor(key, queueSize = 64, queueTTL = 6e4) {
         if (cache[key]) return cache[key]
@@ -17,6 +19,7 @@ class Channel extends EventEmitter {
         this.started = Math.floor(Date.now() / 1e3)
 
         addEventListener('storage', this.onstorage)
+        sleep(shiftInitial(this.queueTTL)).then(this.cleanup)
 
         cache[key] = this
     }
@@ -41,35 +44,58 @@ class Channel extends EventEmitter {
         }
     }
 
+    cleanup = async () => {
+        await lock(this.key, () => {
+            const t = Date.now() - this.queueTTL
+            const queue = this.queueLoad().filter(a => {
+                const objectid = new ObjectId(a.pk)
+                return objectid.generationTime >= t
+            })
+
+            this.seenEvents = new Set(queue
+                .map(a => this.seenEvents.has(a.pk) ? a.pk : null)
+                .filter(a => !!a))
+
+            this.queueSave(queue)
+        })
+        sleep(this.queueTTL).then(this.cleanup)
+    }
+
     send(event, args, { pk, toSelf } = {}) {
         if (!Array.isArray(args)) args = [args]
         if (!pk) pk = (new ObjectId).toString()
 
         this.seenEvents.add(pk)
+        if (toSelf) this.trigger(event, args)
 
         return lock(this.key, () => {
-            let queue
-            try {
-                queue = JSON.parse(localStorage.getItem(`⭕️${this.key}`) || '[]')
-            }
-            catch (err) {
-                queue = []
-            }
+            const queue = this.queueLoad()
 
             queue.push({ pk, event, args })
             while (queue.length > this.queueSize) {
                 this.seenEvents.delete(queue.shift().pk)
             }
 
-            try {
-                localStorage.setItem(`⭕️${this.key}`, JSON.stringify(queue))
-            }
-            catch (err) {
-                console.error(`Channel(${this.key}): localStorage went bad`)
-            }
-
-            if (toSelf) this.trigger(event, args)
+            this.queueSave(queue)
         })
+    }
+
+    queueLoad() {
+        try {
+            return JSON.parse(localStorage.getItem(`⭕️${this.key}`) || '[]')
+        }
+        catch (err) {
+            return []
+        }
+    }
+
+    queueSave(queue) {
+        try {
+            localStorage.setItem(`⭕️${this.key}`, JSON.stringify(queue))
+        }
+        catch (err) {
+            console.error(`Channel(${this.key}): localStorage went bad`)
+        }
     }
 }
 
